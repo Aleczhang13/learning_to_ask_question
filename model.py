2962d631310bf21e453d5c4f6b6aae8d9e89fbeb
chenbinzhang <zcb13601192029@qq.com>
2019-08-25 11:26:17 +0800

    增加decoder部分

--- model.py
+++ model.py
@@ -13,12 +13,21 @@ class Seq2Seq(nn.Module):
                                     bidirectional=True,
                                     drop_prob=drop_out if num_layers > 1 else 0.)
 
-        self.decoder = layer.Decoder()
+        self.decoder = layer.Decoder(input_size=tgt_vocab.vectors.size(1),
+                                     hidden_size=hidden_size,
+                                     word_vector=src_vocab,
+                                     tgt_vocab=tgt_vocab,
+                                     num_layers=num_layers,
+                                     device=device,
+                                     dropout=drop_out if num_layers > 1 else 0.,
+                                     attention=True
+                                     )
 
 
     def forward(self, sentence, len_sentence, qustion, len_question):
         enc_output, enc_hidden= self.encoder(sentence, len_sentence)
-        return enc_output, enc_hidden
+        outputs = self.decoder(enc_output,enc_hidden, qustion)
+        return outputs
 
 
 
